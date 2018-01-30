f48bbd3fafa7a771bcb7779007cabe34bb25725d
Robin Kock <robin@kock-hamburg.de>
2018-01-30 22:07:33 +0100

    wiring form

--- js/index.js
+++ js/index.js
@@ -19,4 +19,19 @@ $(function () {
     $('#navBtn').click(function () {
         $(".navbar-collapse").collapse('toggle');
     });
+    $('#saveBtn').click(function () {
+        var email = $('#emailInput').val();
+        if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email))
+            $.post("http://nodejs-mongo-persistent-rettet-puan-klent.7e14.starter-us-west-2.openshiftapps.com/api/add", {
+                email: email
+            }, function (params, status) {
+                if (params.error) {
+                    $('#emailError').text(params.error);
+                } else {
+                    $('#emailError').text("");
+                }
+            });
+        else
+            $('#emailError').text("Error: not a valid email");
+    });
 });
