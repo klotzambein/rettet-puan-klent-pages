181bb11f6b24b9e6958ef2983cbe98ff95a4cf12
Robin Kock <robin@kock-hamburg.de>
2018-02-01 08:58:53 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -20,18 +20,24 @@ $(function () {
         $(".navbar-collapse").collapse('toggle');
     });
     $('#saveBtn').click(function () {
+        $('#emailError').text("");
+        $('#emailSuccess').text("");
         var email = $('#emailInput').val();
         if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email))
             $.post("http://nodejs-mongo-persistent-rettet-puan-klent.7e14.starter-us-west-2.openshiftapps.com/api/add", {
                 email: email
             }, function (params, status) {
                 if (params.error) {
+                    $('#emailSuccess').text("");
                     $('#emailError').text(params.error);
                 } else {
                     $('#emailError').text("");
+                    $('#emailSuccess').text("Vielen Dank");
                 }
             });
-        else
+        else {
+            $('#emailSuccess').text("");
             $('#emailError').text("Error: not a valid email");
+        }
     });
 });
